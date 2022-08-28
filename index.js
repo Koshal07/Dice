faa6f3970f5ea6da6b71ff1e7ded6eff3925658e
Koshal07 <95358872+Koshal07@users.noreply.github.com>
2022-08-28 14:09:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var randomNumber1=Math.floor((Math.random() *6 )+1); //creating random number fr
 
 var randomDiceImage1="dice" + randomNumber1 +".png"; // creating random dice image from random Number
 
-var randomImageSource1="images/"+randomDiceImage1; //taking dice image source from images folder
+var randomImageSource1=randomDiceImage1; //taking dice image source from images folder
 
 var image1=document.querySelectorAll("img")[0]; //taking out the image of dice 1
 
@@ -11,7 +11,7 @@ image1.setAttribute("src",randomImageSource1); //setting it randomaly
 
 var randomNumber2=Math.floor((Math.random()*6)+1); //creating random number from 1 to 6
 
-var randomImageSource2="images/dice"+randomNumber2 +".png"; //taking dice image source from images folder
+var randomImageSource2="dice"+randomNumber2 +".png"; //taking dice image source from images folder
 
 document.querySelectorAll("img")[1].setAttribute("src" , randomImageSource2);  //taking out the image of dice 2
 
